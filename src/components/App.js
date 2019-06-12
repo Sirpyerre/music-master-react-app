d384b878b688cfab18b923be2b68f81cf39675c2
Pedro Rojas <pedro.rojas@gmail.com>
2019-06-12 16:54:14 -0500

    playing a track

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,43 @@
 import React, {Component} from 'react';
+import Artist from './Artist';
+import Tracks from './Tracks';
+
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
 
 class App extends Component {
-    state = { artistQuery: ''};
+    state = {artistQuery: '', artist: null, tracks:[]};
+
 
     updateArtistQuery = event => {
-        console.log('event.target.value:', event.target.value);
-        this.setState({artistQuery: event.target.value })
+        this.setState({artistQuery: event.target.value})
     };
 
-    handlerKeyPress = event =>{
-        if(event.key === 'Enter') {
+    handlerKeyPress = event => {
+        if (event.key === 'Enter') {
             this.searchArtist();
         }
     };
 
-    searchArtist = () =>{
-        console.log('this.state:', this.state);
+    searchArtist = () => {
+        fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+            .then(response => response.json())
+            .then(json => {
+
+                if (json.artists.total > 0) {
+                    const artist = json.artists.items[0];
+
+                    this.setState({artist});
+
+                    fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                        .then(response => response.json())
+                        .then(json => this.setState({tracks: json.tracks}))
+                        .catch(error => alert(error.message));
+                }
+            }).catch(error => alert(error.message));
     };
 
     render() {
+        console.log('state', this.state);
         return (
             <div>
                 <h2>Music Master</h2>
@@ -28,6 +47,8 @@ class App extends Component {
                     placeholder='Search for an artist'
                 />
                 <button onClick={this.searchArtist}>Seach</button>
+                <Artist artist={this.state.artist}/>
+                <Tracks tracks={this.state.tracks}/>
             </div>
         );
     }
